a1936834a5aa6d891a68f5d2198e9d57559925d1
Muhammad Danish <danishkanirya9999@gmail.com>
2025-10-21 18:34:04 +0500

    adding button navigation bar and adjusting other ui components

--- Aj-Art-Gallery/wwwroot/js/site.js
+++ Aj-Art-Gallery/wwwroot/js/site.js
@@ -17,3 +17,13 @@ window.addEventListener('scroll', () => {
 
     lastScroll = currentScroll;
 });
+document.querySelectorAll('.mobile-nav a').forEach(link => {
+    link.addEventListener('click', async e => {
+        e.preventDefault();
+        const url = e.target.closest('a').href;
+        const res = await fetch(url);
+        const html = await res.text();
+        const newContent = html.match(/<main[\s\S]*<\/main>/)[0];
+        document.querySelector('#main-content').outerHTML = newContent;
+        window.history.pushState({}, '', url);
+    });
